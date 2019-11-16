9040037e85699150ff21763087691a93f31b293f
Ryan M <rmccauleymusic@gmail.com>
2019-11-16 17:09:39 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :anagram
+
+  def initialize(anagram)
+    @anagram = anagram
+  end
+
+  def anagram_detector(anagram)
+    anagram.split(//)==.match(%w())
+  end
+    
+
+end
